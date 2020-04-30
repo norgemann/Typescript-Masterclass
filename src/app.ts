d7cb56cf414b145d0a1cb63f70f65994edfd0afb
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-30 13:37:39 +0200

    Type Queries

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 //“typeof” Type Queries
+//“keyof” Index Type Queries
+//“keyof”, Generics and Lookup Types
 
 const person = {
   name: "Marko",
   age: 33,
+  job: true
 };
-
-type Person = typeof person;
 
 const anotherPerson: Person = {
   name: "Ivan",
   age: 28,
+  job: true
 };
+
+type Person = typeof person;
+type PersonKeys = keyof Person;
+type PersonTypes = Person[PersonKeys];
+
+function getObjProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+console.log(getObjProperty(anotherPerson, 'job'));
