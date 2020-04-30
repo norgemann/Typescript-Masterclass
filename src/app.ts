83b72c875515d56632eacb965ef09e8292da5955
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-30 19:30:39 +0200

    Interfaces vs Type Aliases

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,18 @@
-//Discriminated (Tagged) Unions
-interface Order {
-  id: string;
-  price: number;
-  currency: string;
+//Interfaces vs Type Aliases
+interface Item {
+  name: string;
 }
 
-interface Card {
-  cardNumber: string;
-  cvc: number;
-  type: "type";//Discriminated (Tagged) Unions
-}
-interface Paypal {
-  email: string;
-  type: "paypal";//Discriminated (Tagged) Unions
+interface Artist extends Item {
+  song: string;
 }
 
-type CardOrder = Order & Card;
-type PaypalOrder = Order & Paypal;
+type Artist2 = { song: string } & Item;
 
-const order: Order = {
-  id: "55ffg5",
-  price: 233,
-  currency: "RSD",
-};
+const artist: Artist = { name: "Filip", song: "lala", color: "red" };
+const artist2: Artist2 = { name: "Filip", song: "lala" };
 
-//Intersection Type
-const orderCard: CardOrder = {
-  cardNumber: "4000 52365 9658 2222",
-  cvc: 236,
-  type: "type",
-  ...order,
-};
-
-const orderPaypal: PaypalOrder = {
-  ...order,
-  email: "marko@est.com",
-  type: "paypal",
-};
-
-type Checkout = CardOrder | PaypalOrder;//Discriminated (Tagged) Unions
-
-function checkOut(order: Checkout) {
-  if (order.type === "paypal") {
-    console.log(`Paypal email ${order.email}`);
-  } else {
-    console.log(`Card number${order.cardNumber}`);
-  }
+//i ovako mozes da spojis interfejse, ali ovako ne mogu type-ovi da se spoje!
+interface Artist {
+  color: string;
 }
-
-checkOut(orderCard);
-checkOut(orderPaypal);
