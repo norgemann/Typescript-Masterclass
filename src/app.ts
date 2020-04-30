621d3251842e45d613fd2ac02767dd32cde8209f
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-30 12:15:46 +0200

    Arrow Functions and Lexical Scope

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,26 @@
-//Object literal
-
-const myObj = {
-  myMethod() {
-    console.log("myMethod", this);
-  },
-  myArrowMethod: () => {
-    console.log("myArrowMethod", this);
-  },
-};
-
-//   myObj.myMethod();
-//   myObj.myArrowMethod();
-
-
-
-
-
-//Exploring “this” with .call, .apply and .bind
-
-// function myFunc(text1: string, text2: string) {
-//   console.log("myFunc", this, text1, text2);
-// }
-
-//rest parameter
-function myFunc(...text: string[]) {
-  console.log("myFunc", this, text);
-}
-
-//myFunc("ABC", "DEF");
-const bindFunction = myFunc.bind(myObj);
-bindFunction("ABC", "DEF");
-bindFunction("123", "456");
-bindFunction("ABC", "DEF");
-myFunc.call(myObj, "ABC", "DEF");
-myFunc.apply(myObj, ["ABC", "DEF"]);
-
-// const myArrowFunc = () => {
-//   console.log("myArrowFunc", this);
-// };
-
-//myArrowFunc();
-
-
-
-
-
-
-
-
-
-
+//Arrow Functions and Lexical Scope
 
 //Classes
 
-// class MyClass {
-//   myClassMethod() {
-//     console.log("My Class Method", this);
-//   }
+class MyClass {
+  myClassMethod() {
+    const foo = "foo";
+    console.log("My Class Method", this, foo);
 
-//   myArrowClassMethod = () => {
-//     console.log("myArrowClassMethod", this);
-//   };
-// }
+    setTimeout(function () {
+      console.log("Set Timeout", this, foo);
+    }, 0);
 
-// const myClass = new MyClass();
-// myClass.myClassMethod();
-// myClass.myArrowClassMethod();
+    setTimeout(() => {
+      console.log("Set Timeout Arrow", this, foo);
+    }, 0);
+  }
+
+  // console:
+  // My Class Method MyClass {} foo
+  // Set Timeout Window {parent: Window, opener: null, top: Window, length: 0, frames: Window, …} foo
+  //Set Timeout Arrow MyClass {} foo
+}
+
+const myClass = new MyClass();
+myClass.myClassMethod();
